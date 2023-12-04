38927d064a65e3837161b1c29dd4f206faae1b99
purvag03 <purvag@andrew.cmu.edu>
2023-12-04 13:48:41 -0500

    GraphQL - Component 2

--- src/main/component_2/db.js
+++ src/main/component_2/db.js
@@ -1,38 +1,55 @@
-const persons =[{
-  id: "user001",
-  username: "alice@script.com",
-  age: 34,
-  name: "Alice"
- },{
-  id: "user002",
-  username: "makky@try.com",
-  age: 31,
-  name: "Makky"
- }]
-const posts = [{
-  id: "01",
-  userID: "user001",
-  name: "An introduction to GraphQL"
- },{
-  id: "012",
-  userID: "user002",
-  name: "Javascript for beginners"
- },{
-  id: "03",
-  userID: "user001",
-  name: "Modular GraphQL"
- },{
-  id: "055",
-  userID: "user002",
-  name: "Learn Reactjs"
- },{
-  id: "0123",
-  userID: "user002",
-  name: "Angular vs Reactjs"
-}]
+// Mock database interface 
 
+const PRs = []; // Array to store PRs
+const Comments = []; // Array to store comments
 
 module.exports = {
-persons,
-posts
-}
+    // Fetch a PR by ID
+    getPRById: (id) => PRs.find(pr => pr.id === id),
+
+    // List PRs by status
+    listPRs: (status) => PRs.filter(pr => pr.status === status),
+
+    // Get comments for a PR
+    getComments: (prId) => Comments.filter(comment => comment.prId === prId),
+
+    // Create a PR
+    createPR: (pr) => {
+        PRs.push(pr);
+        return pr;
+    },
+
+    // Add a comment
+    addComment: (comment) => {
+        Comments.push(comment);
+        return comment;
+    },
+
+    // Update a PR's status
+    updatePRStatus: (id, newStatus) => {
+        const pr = PRs.find(pr => pr.id === id);
+        if (pr) {
+            pr.status = newStatus;
+        }
+        return pr;
+    },
+
+    // Merge a PR
+    mergePR: (id) => {
+        const pr = PRs.find(pr => pr.id === id);
+        if (pr && pr.status === 'pending') {
+            pr.status = 'merged';
+        }
+        return pr;
+    },
+
+    // React to a comment
+    reactToComment: (commentId, reaction) => {
+        const comment = Comments.find(c => c.id === commentId);
+        if (comment) {
+            // Add or update reaction count
+            comment.reactions[reaction] = (comment.reactions[reaction] || 0) + 1;
+        }
+        return comment;
+    }
+};
--- src/main/component_2/resolvers.js
+++ src/main/component_2/resolvers.js
@@ -1,17 +1,38 @@
-const db = require('./db')
+const db = require('./db.js');
 
+const resolvers = {
+    Query: {
+        PRDetailsById: (_, { id }) => db.getPRById(id),
+        ListPRs: (_, { status }) => db.listPRs(status),
+        PRComments: (_, { prId }) => db.getComments(prId),
+    },
+    Mutation: {
+        CreatePR: (_, { description, sourceCommit, branchTarget }) => {
+            const newPR = {
+                id: generateID(), // Implement a function to generate unique IDs
+                description,
+                sourceCommit,
+                branchTarget,
+                status: 'pending',
+                comments: []
+            };
+            return db.createPR(newPR);
+        },
+        AddPRComment: (_, { prId, content, lineNumber }) => {
+            const newComment = {
+                id: generateID(), // Implement a function to generate unique IDs
+                prId,
+                content,
+                type: lineNumber ? 'inline' : 'general',
+                lineNumber,
+                reactions: {}
+            };
+            return db.addComment(newComment);
+        },
+        ReactToComment: (_, { commentId, reactionType }) => db.reactToComment(commentId, reactionType),
+        UpdatePRStatus: (_, { prId, newStatus }) => db.updatePRStatus(prId, newStatus),
+        MergePR: (_, { prId }) => db.mergePR(prId),
+    }
+};
 
-const Query = {
- persons: () => db.persons
-}
-
-
-const Person = {
- posts: (person) => db.posts.filter(p => p.userID === person.id)
-}
-
-
-module.exports = {
- Query,
- Person
-}
+module.exports = resolvers;
